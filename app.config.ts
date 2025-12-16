3729decc91cb555b375c892de06a6295893e2299
jesuswhyy <sergeevtimur@gmail.com>
2025-12-16 11:15:10 +0300

    Update app.config.ts

--- app.config.ts
+++ app.config.ts
@@ -1,8 +1,8 @@
 export default defineAppConfig({
-  instagramUrl: 'https://www.instagram.com/quantumbot.kz/',
+  instagramUrl: 'https://www.instagram.com/quantumbot_kz?igsh=dnZnemZ3d2hsZHh3',
   telegramUrl: 'https://t.me/+vOQDGC3VoUJmY2Qy',
   whatsappUrl: 'https://api.whatsapp.com/send/?phone=77473990582&text&type=phone_number&app_absent=0',
-  phoneNumber: '+7 747 477 43 92',
+  phoneNumber: '+7 747 399 05 82',
   email: 'support@quantumbot.kz',
   appUrl: 'https://app.quantumbot.kz',
   videoUrl: 'https://www.youtube.com/embed/qngK9wsEAxg',
