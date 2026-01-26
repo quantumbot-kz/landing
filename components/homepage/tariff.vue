7bd0aa4a4d8b0141ad1cee40b9ff7ddc7c5dc007
jesuswhyy <sergeevtimur@gmail.com>
2026-01-26 15:28:30 +0300

    Update tariff.vue

--- components/homepage/tariff.vue
+++ components/homepage/tariff.vue
@@ -118,7 +118,7 @@ const splideOptions = computed(() => ({
 
       .splide__list {
         display: grid !important;
-        grid-template-columns: repeat(3, 1fr);
+        grid-template-columns: repeat(4, 1fr);
         gap: 32px;
       }
     }
